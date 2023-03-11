422140ad32b03b2cf74e102f77226e580b216e19
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-03-10 23:35:00 -0300

    feat: add ex07

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -7,25 +7,25 @@ void ft_putchar(char c);
 
 void ft_putnbr(int number)
 {
-    if (number == -2147483648)
-    {
-        write(STDOUT_FILENO, "-2147483648", 11);
-        return;
-    }
+	if (number == -2147483648)
+	{
+		write(STDOUT_FILENO, "-2147483648", 11);
+		return;
+	}
 
-    if (number < 0)
-    {
-        ft_putchar('-');
-        number = -number;
-    }
+	if (number < 0)
+	{
+		ft_putchar('-');
+		number = -number;
+	}
 
-    if (number > 9)
-        ft_putnbr(number / 10);
+	if (number > 9)
+		ft_putnbr(number / 10);
 
-    ft_putchar('0' + (number % 10));
+	ft_putchar('0' + (number % 10));
 }
 
 void ft_putchar(char c)
 {
-    write(STDOUT_FILENO, &c, 1);
+	write(STDOUT_FILENO, &c, 1);
 }
