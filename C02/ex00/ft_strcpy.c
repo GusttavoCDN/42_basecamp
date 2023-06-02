f362d34447c2ff2314ddc884d5864cfc8aaf8e5c
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-06-02 01:58:42 -0300

    tests: add tests for ex00 from C02

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,13 +6,15 @@
 /*   By: gusda-si <gusda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 20:56:50 by gusda-si          #+#    #+#             */
-/*   Updated: 2023/05/28 13:03:46 by gusda-si         ###   ########.fr       */
+/*   Updated: 2023/06/01 23:55:06 by gusda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+#include <stdio.h>
+
+char *ft_strcpy(char *dest, char *src)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (src[i] != '\0')
