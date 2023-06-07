183b9d0ecf48a4db05e2e222550659c079972af6
Gustavo Da Silva Amaral Santos <gusda-si@student.42sp.org.br>
2023-06-06 23:09:48 -0300

    feat: I am idiot, just that 2

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: gusda-si <gusda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 13:57:11 by gusda-si          #+#    #+#             */
-/*   Updated: 2023/06/06 00:01:04 by gusda-si         ###   ########.fr       */
+/*   Updated: 2023/06/06 23:09:36 by gusda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 * @return Returns less than 0 if s1 is less than s2, 0 if they are equal,
 * and greater than 0 if s1 is greater than s2
 */
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(const char *s1, const char *s2, int n)
 {
-	unsigned int	i;
-
+	while ((*s1 == *s2) && (*s1 != '\0' && *s2 != '\0') && n > 0)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
 	if (n == 0)
 		return (0);
-	i = 0;
-	while ((s1[i] == s2[i]) && (s1[i] != '\0' && s2[i] != '\0') && i < n)
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
